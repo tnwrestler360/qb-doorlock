de0ded21e9223699fab804ef5838b3ce491b52e8
tnwrestler360 <tyler.davis4494@gmail.com>
2023-09-30 21:24:29 -0700

    initial commit

---
+++ configs/police.lua
@@ -0,0 +1,379 @@
+
+
+-- celldoor1 created by Moscovium
+Config.DoorList['police-celldoor1'] = {
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    objName = -53345114,
+    doorRate = 1.0,
+    doorType = 'door',
+    doorLabel = 'cell1',
+    locked = true,
+    objYaw = 0.0,
+    objCoords = vec3(477.912598, -1012.188660, 26.480055),
+}
+
+-- celldoor2 created by Moscovium
+Config.DoorList['police-celldoor2'] = {
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    objName = -53345114,
+    doorRate = 1.0,
+    doorType = 'door',
+    doorLabel = 'cell2',
+    locked = true,
+    objYaw = 0.0,
+    objCoords = vec3(480.912811, -1012.188660, 26.480055),
+}
+
+-- Mrpd1 created by Moscovium
+Config.DoorList['police-Mrpd1'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd1',
+    doorRate = 1.0,
+    doors = {
+        {objName = -692649124, objYaw = 0.0, objCoords = vec3(467.368622, -1014.406006, 26.483816)},
+        {objName = -692649124, objYaw = 180.00001525879, objCoords = vec3(469.774261, -1014.406006, 26.483816)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
+
+-- Mrpd2 created by Moscovium
+Config.DoorList['police-Mrpd2'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd2',
+    doorRate = 1.0,
+    doors = {
+        {objName = 149284793, objYaw = 89.999977111816, objCoords = vec3(471.375824, -1010.197876, 26.405483)},
+        {objName = 149284793, objYaw = 270.19003295898, objCoords = vec3(471.367859, -1007.793396, 26.405483)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
+
+-- Mrpd3 created by Moscovium
+Config.DoorList['police-Mrpd3'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd3',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(476.615692, -1008.875427, 26.480055),
+    locked = true,
+    objName = -53345114,
+    distance = 2,
+    objYaw = 270.13998413086,
+}
+
+-- Mrpd4 created by Moscovium
+Config.DoorList['police-Mrpd4'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd4',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(483.912720, -1012.188660, 26.480055),
+    locked = true,
+    objName = -53345114,
+    distance = 2,
+    objYaw = 0.0,
+}
+
+-- Mrpd5 created by Moscovium
+Config.DoorList['police-Mrpd5'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd5',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(486.913116, -1012.188660, 26.480055),
+    locked = true,
+    objName = -53345114,
+    distance = 2,
+    objYaw = 0.0,
+}
+
+-- Mrpd6 created by Moscovium
+Config.DoorList['police-Mrpd6'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd6',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(484.176422, -1007.734375, 26.480055),
+    locked = true,
+    objName = -53345114,
+    distance = 2,
+    objYaw = 180.00001525879,
+}
+
+-- Mrpd7 created by Moscovium
+Config.DoorList['police-Mrpd7'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd7',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(481.008362, -1004.117981, 26.480055),
+    locked = true,
+    objName = -53345114,
+    distance = 2,
+    objYaw = 180.00001525879,
+}
+
+-- Mrpd8 created by Moscovium
+Config.DoorList['police-Mrpd8'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd8',
+    doorRate = 1.0,
+    doors = {
+        {objName = 149284793, objYaw = 180.00001525879, objCoords = vec3(482.068573, -997.909973, 26.406504)},
+        {objName = 149284793, objYaw = 0.0, objCoords = vec3(479.663757, -997.909973, 26.406504)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
+
+-- Mrpd9 created by Moscovium
+Config.DoorList['police-Mrpd9'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd9',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(482.670258, -995.728516, 26.405483),
+    locked = true,
+    objName = -1406685646,
+    distance = 2,
+    objYaw = 270.00003051758,
+}
+
+-- Mrpd10 created by Moscovium
+Config.DoorList['police-Mrpd10'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd10',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(482.669922, -992.299133, 26.405483),
+    locked = true,
+    objName = -1406685646,
+    distance = 2,
+    objYaw = 270.00003051758,
+}
+
+-- Mrpd11 created by Moscovium
+Config.DoorList['police-Mrpd11'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd11',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(482.670135, -987.579163, 26.405483),
+    locked = true,
+    objName = -1406685646,
+    distance = 2,
+    objYaw = 270.00003051758,
+}
+
+-- Mrpd12 created by Moscovium
+Config.DoorList['police-Mrpd12'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd12',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(482.669434, -983.986816, 26.405483),
+    locked = true,
+    objName = -1406685646,
+    distance = 2,
+    objYaw = 270.00003051758,
+}
+
+-- Mrpd13 created by Moscovium
+Config.DoorList['police-Mrpd13'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd13',
+    doorRate = 1.0,
+    doors = {
+        {objName = 149284793, objYaw = 270.00003051758, objCoords = vec3(479.062408, -985.032349, 26.405483)},
+        {objName = 149284793, objYaw = 89.999977111816, objCoords = vec3(479.062408, -987.437561, 26.405483)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
+
+-- Mrpd14 created by Moscovium
+Config.DoorList['police-Mrpd14'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd14',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(478.289154, -997.910095, 26.405483),
+    locked = true,
+    objName = 149284793,
+    distance = 2,
+    objYaw = 180.00001525879,
+}
+
+-- Mrpd15 created by Moscovium
+Config.DoorList['police-Mrpd15'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd15',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(475.832336, -990.483948, 26.405483),
+    locked = true,
+    objName = -692649124,
+    distance = 2,
+    objYaw = 134.97177124023,
+}
+
+-- Mrpd16 created by Moscovium
+Config.DoorList['police-Mrpd16'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd16',
+    doorRate = 1.0,
+    doors = {
+        {objName = -96679321, objYaw = 270.00003051758, objCoords = vec3(471.375305, -987.437378, 26.405483)},
+        {objName = -1406685646, objYaw = 270.00003051758, objCoords = vec3(471.375305, -985.031921, 26.405483)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
+
+-- Mrpd17 created by Moscovium
+Config.DoorList['police-Mrpd17'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd17',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(464.159058, -974.665588, 26.370705),
+    locked = true,
+    objName = 1830360419,
+    distance = 2,
+    objYaw = 269.79000854492,
+}
+
+-- Mrpd18 created by Moscovium
+Config.DoorList['police-Mrpd18'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd18',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(464.156555, -997.509277, 26.370705),
+    locked = true,
+    objName = 1830360419,
+    distance = 2,
+    objYaw = 89.870010375977,
+}
+
+-- Mrpd19 created by Moscovium
+Config.DoorList['police-Mrpd19'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd19',
+    doorRate = 1.0,
+    doors = {
+        {objName = -288803980, objYaw = 0.0, objCoords = vec3(467.522217, -1000.543701, 26.405483)},
+        {objName = -288803980, objYaw = 180.00001525879, objCoords = vec3(469.927368, -1000.543701, 26.405483)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
+
+-- Mrpd20 created by Moscovium
+Config.DoorList['police-Mrpd20'] = {
+    doorType = 'garage',
+    doorLabel = 'Mrpd20',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(452.300507, -1000.771667, 26.696609),
+    locked = true,
+    objName = 2130672747,
+    distance = 5,
+    objYaw = 0.0,
+}
+
+-- Mrpd21 created by Moscovium
+Config.DoorList['police-Mrpd21'] = {
+    doorType = 'garage',
+    doorLabel = 'Mrpd21',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(431.411926, -1000.771667, 26.696609),
+    locked = true,
+    objName = 2130672747,
+    distance = 5,
+    objYaw = 0.0,
+}
+
+-- Mrpd22 created by Moscovium
+Config.DoorList['police-Mrpd22'] = {
+    doorType = 'garage',
+    doorLabel = 'Mrpd22',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(410.025787, -1028.318970, 28.418146),
+    locked = true,
+    objName = -1635161509,
+    distance = 5,
+    objYaw = 270.0,
+}
+
+-- Mrpd24 created by Moscovium
+Config.DoorList['police-Mrpd24'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd24',
+    doorRate = 1.0,
+    doors = {
+        {objName = -1547307588, objYaw = 180.00001525879, objCoords = vec3(443.061768, -998.746216, 30.815304)},
+        {objName = -1547307588, objYaw = 0.0, objCoords = vec3(440.739197, -998.746216, 30.815304)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
+
+-- Mrpd25 created by Moscovium
+Config.DoorList['police-Mrpd25'] = {
+    doorType = 'door',
+    doorLabel = 'Mrpd25',
+    doorRate = 1.0,
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objCoords = vec3(440.520081, -986.233459, 30.823193),
+    locked = true,
+    objName = -96679321,
+    distance = 2,
+    objYaw = 180.00001525879,
+}
+
+-- Mrpd26 created by Moscovium
+Config.DoorList['police-Mrpd26'] = {
+    doorType = 'double',
+    doorLabel = 'Mrpd26',
+    doorRate = 1.0,
+    doors = {
+        {objName = -288803980, objYaw = 270.00003051758, objCoords = vec3(469.440613, -985.031311, 30.823193)},
+        {objName = -288803980, objYaw = 89.999977111816, objCoords = vec3(469.440613, -987.437683, 30.823193)}
+    },
+    authorizedJobs = { ['police'] = 0 },
+    distance = 2,
+    locked = true,
+}
